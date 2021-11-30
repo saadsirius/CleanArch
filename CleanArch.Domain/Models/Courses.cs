6570128be297cf584bc23a12f6e4c4756da53937
saadlamkadam <saad.lamkadam@gmail.com>
2021-12-01 00:29:40 +0100

    Add Course(Domain Model)

---
+++ CleanArch.Domain/Models/Courses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Models
+{
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+
+    }
+}
