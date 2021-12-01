0e013b33cae1f9241e16b73c92d8ca5fab3457b9
saadlamkadam <saad.lamkadam@gmail.com>
2021-12-01 02:12:06 +0100

    Creation of Icourse Service and CourseModelView

--- CleanArch.Domain/Models/Course.cs
+++ CleanArch.Domain/Models/Course.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CleanArch.Domain.Models
 {
-    public class Courses
+    public class Course
     {
         public int Id { get; set; }
         public string Name { get; set; }
